5a782b3e137b9569e36d3e13e02da9e621fa30e9
ldschramel <ldschramel@nx.csbsju.edu>
2016-03-14 21:07:48 -0500

    yet another test 123

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+//This is a test message!!!!!! Delete eventually!!!!!!!
 /**
  * This class defines the attributes associated with and creates an instance of an Admin for the CMC System
  * @author smcarik, Adam Noack, Curtis Noecker, Logan Schramel
