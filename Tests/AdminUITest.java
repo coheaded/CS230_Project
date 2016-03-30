9fc4b40ad5e6137789a828596497e83e4a2f4149
ccnoecker <ccnoecker@csbsju.edu>
2016-03-29 19:05:34 -0500

    Added AdminUITest class inside Tests source folder

---
+++ Tests/AdminUITest.java
@@ -0,0 +1,62 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdminUITest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdminUI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChooseEdited() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditUniversity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplaySchool() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewPeople() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewSchools() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllUs() {
+		fail("Not yet implemented");
+	}
+
+}
