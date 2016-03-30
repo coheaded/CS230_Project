7717bca945e09f7c27d0c2e6843ed55365efa257
ccnoecker <ccnoecker@csbsju.edu>
2016-03-29 20:28:50 -0500

    Added more to AdminUITest

--- Tests/AdminUITest.java
+++ Tests/AdminUITest.java
@@ -2,61 +2,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import uis.*;
 
 public class AdminUITest {
+	AdminUI admin;
 
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@Test
-	public void testAdminUI() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddPerson() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEditPerson() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testChooseEdited() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddUniversity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEditUniversity() {
-		fail("Not yet implemented");
+		admin = new AdminUI("juser");
 	}
 
 	@Test
 	public void testDisplaySchool() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testViewPeople() {
-		fail("Not yet implemented");
+		String[] expected = {"EMORY","GEORGIA","URBAN","PRIVATE","15000","45","550",
+				"600","34980","35","5500","35","30","4","4","4"};
+		String[] result = admin.displaySchool("Emory");
+		assertTrue("Displaying Emory",result.equals(expected));
 	}
 
 	@Test
 	public void testViewSchools() {
-		fail("Not yet implemented");
+		String[][] array = admin.viewSchools();
+		String[] expected = {"ABILENE CHRISTIAN UNIVERSITY","TEXAS","SUBURBAN","PRIVATE","10000","50","-1","-1","12088","70","4000","90","80","2","3","3",
+				"LIBERAL-ARTS"};
+		assertTrue("Viewing Abilene Christian University",array[0].equals(expected));
+		
 	}
 
-	@Test
-	public void testGetAllUs() {
-		fail("Not yet implemented");
-	}
 
 }
